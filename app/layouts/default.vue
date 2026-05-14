20e0d0629cdb7d17be7a8bd65336917a3514909e
Евгений <egoji@mail.ru>
2026-05-15 00:34:25 +0300

    test

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="flex flex-col relative min-h-[100vh] s-layout">
+    <div class="headerex">
+      <div class="headerex__content">
+        <div class="headerex__item">ru</div>
+        <div class="headerex__item">ru</div>
+      </div>
+    </div>
     <main class="flex-1 main">
       <slot />
     </main>
